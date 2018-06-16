46afe32548dc122e405849bf21ea938580080e5b
Anton <anton@adc.sh>
2018-06-16 12:18:01 +0100

    fix npm rule

--- src/lib/rules.js
+++ src/lib/rules.js
@@ -1,5 +1,5 @@
 export const badgeRule = {
-  re: /^%NPM: (\w\d-_+)$/gm,
+  re: /^%NPM: ([\w\d-_]+)%$/gm,
   replacement(match, name) {
     return `[![npm version](https://badge.fury.io/js/${name}.svg)](https://npmjs.org/package/${name})`
   },
