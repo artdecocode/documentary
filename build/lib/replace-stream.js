0c7daa5c96ed4f060e9779f6a8d983a2d42cba27
Anton <anton@adc.sh>
2018-06-21 10:40:07 +0100

    build

--- build/lib/replace-stream.js
+++ build/lib/replace-stream.js
@@ -7,6 +7,8 @@ exports.default = createReplaceStream;
 
 var _restream = require("restream");
 
+var _util = require("util");
+
 var _rules = require("./rules");
 
 var _lib = require("../lib");
@@ -17,7 +19,11 @@ var _methodTitle = _interopRequireDefault(require("./rules/method-title"));
 
 var _example = _interopRequireDefault(require("./rules/example"));
 
+var _spawncommand = _interopRequireDefault(require("spawncommand"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const LOG = (0, _util.debuglog)('doc');
 
 function createReplaceStream(toc) {
   const tocRule = (0, _rules.createTocRule)(toc);
@@ -35,6 +41,35 @@ function createReplaceStream(toc) {
       return marker;
     }
 
+  }, {
+    re: /%TREE (.+)%/mg,
+
+    async replacement(match, m) {
+      const args = m.split(' ');
+      const p = (0, _spawncommand.default)('tree', ['--noreport', ...args]);
+
+      try {
+        const {
+          stdout
+        } = await p.promise;
+
+        if (/\[error opening dir\]/.test(stdout)) {
+          LOG('Could not generate a tree for %s', args.join(' '));
+          return match;
+        }
+
+        return escape(stdout);
+      } catch (err) {
+        if (err.code == 'ENOENT') {
+          console.warn('tree is not installed');
+          return match;
+        }
+
+        LOG(err.message);
+        return match;
+      }
+    }
+
   }, tocRule, _rules.badgeRule, _table.default, _methodTitle.default, _example.default, {
     re: new RegExp(marker, 'g'),
 
@@ -45,4 +80,6 @@ function createReplaceStream(toc) {
   }]);
   return s;
 }
+
+const escape = m => `\`\`\`m\n${m.trim()}\n\`\`\``;
 //# sourceMappingURL=replace-stream.js.map
