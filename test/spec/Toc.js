9496d8b0f420b29b7849ef1cf6b2ab72395d9c39
Anton <anton@adc.sh>
2018-06-21 07:23:53 +0100

    toc test

---
+++ test/spec/Toc.js
@@ -0,0 +1,22 @@
+import Context from '../context'
+import SnapshotContext from 'snapshot-context'
+import { createReadStream } from 'fs'
+import Toc from '../../src/lib/Toc'
+
+/** @type {Object.<string, (c: Context, s: SnapshotContext )>} */
+const T = {
+  context: [
+    Context,
+    SnapshotContext,
+  ],
+  async 'reads TOC'({ SNAPSHOT_DIR, README_PATH, catchment }, { setDir, test }) {
+    setDir(SNAPSHOT_DIR)
+    const toc = new Toc()
+    const rs = createReadStream(README_PATH)
+    rs.pipe(toc)
+    const res = await catchment(toc)
+    await test('bin/toc.md', res)
+  },
+}
+
+export default T
