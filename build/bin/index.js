d9ba8a87e19bd6433175433fbd70844184777354
Anton <anton@adc.sh>
2018-07-17 07:25:14 +0100

    build

--- build/bin/index.js
+++ build/bin/index.js
@@ -9,6 +9,8 @@ var _pedantry = _interopRequireDefault(require("pedantry"));
 
 var _util = require("util");
 
+var _spawncommand = _interopRequireDefault(require("spawncommand"));
+
 var _run = _interopRequireDefault(require("./run"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
@@ -19,7 +21,8 @@ const {
   source: _source,
   output: _output,
   toc: _toc,
-  watch: _watch
+  watch: _watch,
+  push: _push
 } = (0, _argufy.default)({
   source: {
     command: true
@@ -32,8 +35,16 @@ const {
     short: 'w',
     boolean: true
   },
-  output: 'o'
+  output: 'o',
+  push: {
+    short: 'p'
+  }
 });
+
+if (process.argv.find(a => a == '-p') && !_push) {
+  console.log('Please specify a commit message.');
+  process.exit(1);
+}
 
 const doc = async (source, output, justToc = false) => {
   if (!source) {
@@ -72,13 +83,19 @@ const doc = async (source, output, justToc = false) => {
 
   let debounce = false;
 
-  if (_watch) {
+  if (_watch || _push) {
     (0, _fs.watch)(_source, {
       recursive: true
     }, async () => {
       if (!debounce) {
         debounce = true;
         await doc(_source, _output, _toc);
+
+        if (_push) {
+          console.log('Pushing documentation changes');
+          await gitPush(_source, _output, _push);
+        }
+
         setTimeout(() => {
           debounce = false;
         }, 100);
@@ -86,4 +103,31 @@ const doc = async (source, output, justToc = false) => {
     });
   }
 })();
+
+const gitPush = async (source, output, message) => {
+  const {
+    promise
+  } = (0, _spawncommand.default)('git', ['log', '--format=%B', '-n', '1']);
+  const {
+    stdout
+  } = await promise;
+  const s = stdout.trim();
+
+  if (s == message) {
+    await git('reset', 'HEAD~1');
+  }
+
+  await git('add', source, output);
+  await git('commit', '-m', message);
+  await git('push', '-f');
+};
+
+const git = async (...args) => {
+  const {
+    promise
+  } = (0, _spawncommand.default)('git', args, {
+    stdio: 'inherit'
+  });
+  await promise;
+};
 //# sourceMappingURL=index.js.map
