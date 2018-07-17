1f74bb3eff574dba5ade68346053d2a131b2d1c4
Anton <anton@adc.sh>
2018-07-17 07:02:56 +0100

    implement -p

--- src/bin/index.js
+++ src/bin/index.js
@@ -4,6 +4,7 @@ import argufy from 'argufy'
 import { lstatSync, createReadStream } from 'fs'
 import Pedantry from 'pedantry'
 import { debuglog } from 'util'
+import spawn from 'spawncommand'
 import run from './run'
 
 const LOG = debuglog('doc')
@@ -14,6 +15,7 @@ const {
   output: _output,
   toc: _toc,
   watch: _watch,
+  push: _push,
 } = argufy({
   source: {
     command: true,
@@ -27,7 +29,15 @@ const {
     boolean: true,
   },
   output: 'o',
+  push: {
+    short: 'p',
+  },
 })
+
+if (process.argv.find(a => a == '-p') && !_push) {
+  console.log('Please specify a commit message.')
+  process.exit(1)
+}
 
 const doc = async (source, output, justToc = false) => {
   if (!source) {
@@ -60,8 +70,21 @@ const doc = async (source, output, justToc = false) => {
       if (!debounce) {
         debounce = true
         await doc(_source, _output, _toc)
+        if (_push) {
+          await gitPush(_source, _output, _push)
+        }
         setTimeout(() => { debounce = false }, 100)
       }
     })
   }
 })()
+
+const gitPush = async (source, output, message) => {
+  const { stdout } = (await spawn('git', ['log', '--format=%B', '-n', '1'])).trim()
+  if (stdout == message) {
+    await spawn('git', ['reset', 'HEAD~1'])
+  }
+  await spawn('git', ['add', source, output])
+  await spawn('git', ['commit', '-m', message])
+  await spawn('git', ['push', '-f'])
+}
