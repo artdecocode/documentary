991c42d25cd15243baf82c64c328bf8c4d2b4d00
Anton <anton@adc.sh>
2018-07-18 01:39:14 +0100

    example row tests

--- test/spec/replace-stream/type.js
+++ test/spec/replace-stream/type.js
@@ -34,6 +34,30 @@ ${type}
     const res = await catchment(stream)
     await test('replace-stream/type-headings.txt', res)
   },
+  async 'produces an example row'(
+    { createReadable, catchment, SNAPSHOT_DIR }, { setDir, test }
+  ) {
+    setDir(SNAPSHOT_DIR)
+    const s = `%TYPE
+  <p name="headers" type="object">
+  <d>Incoming headers returned by the server.</d>
+  <e row><code>
+{
+  "server": "GitHub.com",
+  "content-type": "application/json",
+  "content-length": "2",
+  "connection": "close",
+  "status": "200 OK"
+}
+   </code></e>
+</p>
+%`
+    const rs = createReadable(s)
+    const stream = createReplaceStream()
+    rs.pipe(stream)
+    const res = await catchment(stream)
+    await test('replace-stream/type-example-row.txt', res)
+  },
 }
 
 export default T
