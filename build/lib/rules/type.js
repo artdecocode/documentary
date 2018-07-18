2671eceeed3f61c6f5bbbe6a83d92ac763e41ce9
Anton <anton@adc.sh>
2018-07-18 22:14:45 +0100

    build

--- build/lib/rules/type.js
+++ build/lib/rules/type.js
@@ -3,79 +3,17 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = exports.extractTag = exports.typeRe = void 0;
+exports.default = exports.typeRe = void 0;
 
 var _util = require("util");
+
+var _rexml = _interopRequireDefault(require("rexml"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const LOG = (0, _util.debuglog)('doc');
 const typeRe = /^%TYPE( .+)?\n([\s\S]+?)\n%$/mg;
 exports.typeRe = typeRe;
-
-const execRes = (re, s) => {
-  const res = re.exec(s);
-  if (!res) return res;
-  const [, ...args] = res;
-  return args;
-};
-
-const extractProps = s => {
-  const propsRe = /(\w+)="(.*?)"/g;
-  let t;
-  const r = [];
-
-  while ((t = propsRe.exec(s)) !== null) {
-    const [, key, value] = t;
-    r.push({
-      key,
-      value: getPropValue(value)
-    });
-  }
-
-  const tt = s.replace(propsRe, '').trim().split(' ').filter(a => a).reduce((a, k) => ({ ...a,
-    [k]: true
-  }), {});
-  return r.reduce((acc, {
-    key,
-    value
-  }) => ({ ...acc,
-    [key]: value
-  }), tt);
-};
-
-const getPropValue = val => {
-  if (val == 'true') return true;
-  if (val == 'false') return false;
-
-  if (/^\d+$/.test(val)) {
-    return parseInt(val, 10);
-  }
-
-  return val;
-};
-
-const extractTag = (tag, string) => {
-  const re = new RegExp(`<${tag}( .[^>]+)?(?: /)?>(?:([\\s\\S]+?)</${tag}>)?`, 'g');
-  const r = [];
-  let t;
-
-  while ((t = execRes(re, string)) !== null) {
-    if (!t.length) continue;
-    const [_p = '', _c = ''] = t;
-
-    const p = _p.replace(/\/$/, '').trim();
-
-    const props = extractProps(p);
-    const item = {
-      props,
-      content: _c
-    };
-    r.push(item);
-  }
-
-  return r;
-};
-
-exports.extractTag = extractTag;
 
 const tag = (t, s) => `<${t}>${s}</${t}>`;
 
@@ -143,19 +81,19 @@ const typeRule = {
 
   replacement(match, tocTitles, body) {
     try {
-      const tags = extractTag('p', body).map(({
+      const tags = (0, _rexml.default)('p', body).map(({
         content,
         props
       }) => {
         const [{
           content: description
-        } = {}] = extractTag('d', content);
+        } = {}] = (0, _rexml.default)('d', content);
         const [{
           content: example,
           props: {
             row: isExampleRow = false
           } = {}
-        } = {}] = extractTag('e', content);
+        } = {}] = (0, _rexml.default)('e', content);
         return {
           description,
           example,
