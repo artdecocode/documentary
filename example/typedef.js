d5297145b4beb6a27b84a99dc4e709c52bf2f5ff
Anton <anton@adc.sh>
2018-07-25 20:12:07 +0100

    typedef example

--- example/typedef.js
+++ example/typedef.js
@@ -3,13 +3,32 @@ import Static from 'koa-static'
 
 /**
  * Configure the middleware.
- * @param {StaticConfig} config
+ * @param {StaticConfig} config Options to setup `koa-static`.
+ * @param {string} config.root Root directory string.
+ * @param {number} [config.maxage="0"] Browser cache max-age in milliseconds. Default `0`.
+ * @param {boolean} [config.hidden="false"] Allow transfer of hidden files. Default `false`.
+ * @param {string} [config.index="index.html"] Default file name. Default `index.html`.
+ * @param {SetHeaders} [config.setHeaders] Function to set custom headers on response.
  */
 function configure(config) {
   const middleware = Static(config)
   return middleware
 }
 
+// configure({
+//   setHeader
+// })
+
 /* documentary types/static.xml */
+/**
+ * @typedef {import('http').ServerResponse} ServerResponse
+ * @typedef {(res: ServerResponse) => any} SetHeaders Function to set custom headers on response.
+ * @typedef {Object} StaticConfig Options to setup `koa-static`.
+ * @prop {string} root Root directory string.
+ * @prop {number} [maxage=0] Browser cache max-age in milliseconds. Default `0`.
+ * @prop {boolean} [hidden=false] Allow transfer of hidden files. Default `false`.
+ * @prop {string} [index="index.html"] Default file name. Default `index.html`.
+ * @prop {SetHeaders} [setHeaders] Function to set custom headers on response.
+ */
 
 export default configure
