180a3be3b683f41a3f49521431010af9a23542cb
Anton <anton@adc.sh>
2018-07-26 23:57:31 +0100

    print saved to output when -T -o is given

--- src/bin/run-js.js
+++ src/bin/run-js.js
@@ -1,12 +1,12 @@
 import { createWriteStream, createReadStream } from 'fs'
-import createJsReplaceStream from '../lib/js-replace-stream'
 import Catchment from 'catchment'
+import createJsReplaceStream from '../lib/js-replace-stream'
 
 /**
  * Process a JavaScript file.
  * @param {string} source Path to the source JavaScript file.
  */
-export default async function runJs(source, output) {
+export default async function runJs(source, output = source) {
   const s = createReadStream(source)
   const rs = createJsReplaceStream()
   s.pipe(rs)
@@ -14,9 +14,9 @@ export default async function runJs(source, output) {
     rs,
   })
   const res = await c.promise
-  const ws = createWriteStream(output || source)
+  const ws = createWriteStream(output)
   await new Promise((r, j) => {
     ws.on('error', j).end(res, r)
   })
-  console.log('Updated %s to include types.', source)
+  console.log(...(source == output ? ['Updated %s to include types.', source] : ['Saved output to %s', output]))
 }
