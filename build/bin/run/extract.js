25ca095dba8149975f0992f3d9ce35e72ab37432
Anton <anton@adc.sh>
2018-07-27 20:38:20 +0100

    b

--- build/bin/run/extract.js
+++ build/bin/run/extract.js
@@ -3,8 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = runExtract;
-exports.propExtractRe = void 0;
+exports.default = exports.propExtractRe = void 0;
 
 var _fs = require("fs");
 
@@ -39,7 +38,9 @@ const keys = ['type', 'opt', 'name', 'quote', 'defaultValue', 'description', 'De
 const makeT = (type, name, description, properties) => {
   const hasProps = properties.length;
   const tt = type && type != 'Object' ? ` type="${type}"` : '';
-  const t = `  <t name="${name}"${tt} desc="${description}"${hasProps ? '' : ' /'}>\n`;
+  const d = description ? ` desc="${description}"` : '';
+  const i = ' '.repeat(2);
+  const t = `${i}<t name="${name}"${tt}${d}${hasProps ? '' : ' /'}>\n`;
   return t;
 };
 
@@ -48,12 +49,33 @@ const makeP = (type, name, defaultValue, optional, description) => {
   const def = defaultValue !== undefined ? ` default="${defaultValue}"` : '';
   const o = optional ? ' opt' : '';
   const desc = description ? `>${description}</p>` : '/>';
-  const p = `    <p${o}${t} name="${name}"${def}${desc}\n`;
+  const i = ' '.repeat(4);
+  const p = `${i}<p${o}${t} name="${name}"${def}${desc}\n`;
   return p;
 };
 
-const XML = new _stream.Transform({
-  transform({
+const writeOnce = async (stream, data) => {
+  let jj;
+  await new Promise((r, j) => {
+    jj = j;
+    stream.on('error', jj);
+    stream.write(data, r);
+  });
+  stream.removeListener('error', jj);
+};
+/**
+ * Writes XML.
+ */
+
+
+class XML extends _stream.Transform {
+  constructor() {
+    super({
+      writableObjectMode: true
+    });
+  }
+
+  _transform({
     type,
     name,
     description,
@@ -73,92 +95,101 @@ const XML = new _stream.Transform({
     });
     if (properties.length) this.push('  </t>\n');
     next();
-  },
+  }
 
-  writableObjectMode: true
-});
-
-const writeOnce = async (stream, data) => {
-  let jj;
-  await new Promise((r, j) => {
-    jj = j;
-    stream.on('error', jj);
-    stream.write(data, r);
-  });
-  stream.removeListener('error', jj);
-};
+}
 /**
- * Process a JavaScript file.
- * @param {string} source Path to the source JavaScript file.
+ * Parses properties from a RegExp stream.
  */
 
 
-async function runExtract({
-  source,
-  extract = '-'
-}) {
+class Properties extends _stream.Transform {
+  constructor() {
+    super({
+      objectMode: true
+    });
+  }
+
+  _transform([, type, name, description, props], _, next) {
+    /** @type {Object.<string, string>[]} */
+    const p = (0, _mismatch.default)(propExtractRe, props, keys);
+    const properties = p.map(e => {
+      const {
+        defaultValue: d,
+        Default: D,
+        opt: o,
+        ...rest
+      } = e;
+      const pr = { ...rest,
+        ...(d ? {
+          defaultValue: getVal(d)
+        } : {}),
+        ...(D ? {
+          Default: getVal(D)
+        } : {}),
+        ...(o ? {
+          optional: true
+        } : {})
+      };
+
+      if (d || D) {
+        if (!d) {
+          const dn = (0, _typedef.getNameWithDefault)(pr.name, D, pr.type);
+          LOG('%s[%s] got from Default.', name, dn);
+        } else if (d != D) {
+          const dn = (0, _typedef.getNameWithDefault)(pr.name, D, pr.type);
+          LOG('%s[%s] does not match Default `%s`.', name, dn, pr.Default);
+        }
+
+        pr.default = 'defaultValue' in pr ? pr.defaultValue : pr.Default;
+        delete pr.defaultValue;
+        delete pr.Default;
+      }
+
+      return pr;
+    });
+    const o = {
+      type,
+      name,
+      description,
+      properties
+    };
+    this.push(o);
+    next();
+  }
+
+}
+/**
+ * Process a JavaScript file to extract typedefs and place them in an XML file.
+ * @param {Config} config Configuration object.
+ * @param {string} config.source Input file from which to extract typedefs.
+ * @param {string} [config.extract="-"] Output file to where to write XML. `-` will write to `stdout`.
+ * @param {string} [config.stream] An output stream to which to write instead of a location from `extract`.
+ */
+
+
+async function runExtract(config) {
+  const {
+    source,
+    extract = '-',
+    stream: st
+  } = config;
+
   try {
     const s = (0, _fs.createReadStream)(source);
     const ts = (0, _restream.default)(_re.default);
-    const rs = new _stream.Transform({
-      transform([, type, name, description, props], _, next) {
-        /** @type {Object.<string, string>[]} */
-        const p = (0, _mismatch.default)(propExtractRe, props, keys);
-        const properties = p.map(e => {
-          const {
-            defaultValue: d,
-            Default: D,
-            opt: o,
-            ...rest
-          } = e;
-          const pr = { ...rest,
-            ...(d ? {
-              defaultValue: getVal(d)
-            } : {}),
-            ...(D ? {
-              Default: getVal(D)
-            } : {}),
-            ...(o ? {
-              optional: true
-            } : {})
-          };
-
-          if (d || D) {
-            if (!d) {
-              const dn = (0, _typedef.getNameWithDefault)(pr.name, D, pr.type);
-              LOG('%s[%s] got from Default.', name, dn);
-            } else if (d != D) {
-              const dn = (0, _typedef.getNameWithDefault)(pr.name, D, pr.type);
-              LOG('%s[%s] does not match Default `%s`.', name, dn, pr.Default);
-            }
-
-            pr.default = 'defaultValue' in pr ? pr.defaultValue : pr.Default;
-            delete pr.defaultValue;
-            delete pr.Default;
-          }
-
-          return pr;
-        });
-        const o = {
-          type,
-          name,
-          description,
-          properties
-        };
-        this.push(o);
-        next();
-      },
-
-      objectMode: true
-    });
+    const ps = new Properties();
     const stream = new _stream.PassThrough();
+    const xml = new XML();
     await writeOnce(stream, '<types>\n');
-    s.pipe(ts).pipe(rs).pipe(XML).pipe(stream, {
+    s.pipe(ts).pipe(ps).pipe(xml).pipe(stream, {
       end: false
     });
     let p = Promise.resolve();
 
-    if (extract == '-') {
+    if (st) {
+      stream.pipe(st);
+    } else if (extract == '-') {
       stream.pipe(process.stdout);
     } else {
       const ws = (0, _fs.createWriteStream)(extract);
@@ -170,19 +201,19 @@ async function runExtract({
     }
 
     await new Promise((r, j) => {
-      rs.on('error', e => {
-        LOG('Error in RegexTransform');
+      s.on('error', e => {
+        LOG('Error in Read');
         j(e);
       });
       ts.on('error', e => {
         LOG('Error in Transform');
         j(e);
       });
-      s.on('error', e => {
-        LOG('Error in Read');
+      ps.on('error', e => {
+        LOG('Error in RegexTransform');
         j(e);
       });
-      XML.on('error', e => {
+      xml.on('error', e => {
         LOG('Error in XML');
         j(e);
       });
@@ -190,12 +221,22 @@ async function runExtract({
         LOG('Error in Stream');
         j(e);
       });
-      XML.on('end', r);
+      xml.on('end', r);
     });
-    await writeOnce(stream, '</types>\n');
+    await new Promise(r => stream.end('</types>\n', r));
     await p;
   } catch (err) {
     (0, _catcher.default)(err);
   }
 }
+/**
+ * @typedef {Object} Config Configuration object.
+ * @prop {string} [source] Input file from which to extract typedefs.
+ * @prop {string} [extract="-"] Output file to where to write XML. `-` will write to `stdout`.
+ * @prop {Readable} [stream] An output stream to which to write instead of a location from `extract`.
+ */
+
+
+var _default = runExtract;
+exports.default = _default;
 //# sourceMappingURL=extract.js.map
