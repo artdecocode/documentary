d35f6b8d49c684eedc998d878cbecabae9b5a9d8
Anton <anton@adc.sh>
2018-07-27 19:47:28 +0100

    catcher

---
+++ src/bin/catcher.js
@@ -0,0 +1,18 @@
+import { debuglog } from 'util'
+
+const LOG = debuglog('doc')
+const DEBUG = /doc/.test(process.env.NODE_DEBUG)
+
+const catcher = async (err) => {
+  let stack
+  let message
+  if (err instanceof Error) {
+    ({ stack, message } = err)
+  } else {
+    stack = message = err
+  }
+  DEBUG ? LOG(stack) : console.log(message)
+  process.exit(1)
+}
+
+export default catcher
--- src/bin/run/js.js
+++ src/bin/run/js.js
@@ -1,6 +1,7 @@
 import { createWriteStream, createReadStream } from 'fs'
 import Catchment from 'catchment'
-import createJsReplaceStream from '../lib/js-replace-stream'
+import createJsReplaceStream from '../../lib/js-replace-stream'
+import catcher from '../catcher'
 
 /**
  * Process a JavaScript file.
@@ -10,16 +11,25 @@ export default async function runJs({
   source,
   output = source,
 }) {
-  const s = createReadStream(source)
-  const rs = createJsReplaceStream()
-  s.pipe(rs)
-  const c = new Catchment({
-    rs,
-  })
-  const res = await c.promise
-  const ws = createWriteStream(output)
-  await new Promise((r, j) => {
-    ws.on('error', j).end(res, r)
-  })
-  console.log(...(source == output ? ['Updated %s to include types.', source] : ['Saved output to %s', output]))
+  try {
+    if (!source) {
+      console.log('Please specify a JavaScript file.')
+      process.exit(1)
+    }
+
+    const s = createReadStream(source)
+    const rs = createJsReplaceStream()
+    s.pipe(rs)
+    const c = new Catchment({
+      rs,
+    })
+    const res = await c.promise
+    const ws = createWriteStream(output)
+    await new Promise((r, j) => {
+      ws.on('error', j).end(res, r)
+    })
+    console.log(...(source == output ? ['Updated %s to include types.', source] : ['Saved output to %s', output]))
+  } catch (err) {
+    catcher(err)
+  }
 }
