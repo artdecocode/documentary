ebd26a7f1f1a598c1559131ec867b3e8679d02ad
Anton <anton@adc.sh>
2018-09-12 20:31:54 +0300

    implement macro rule

--- src/lib/rules/table.js
+++ src/lib/rules/table.js
@@ -2,15 +2,17 @@ import { debuglog } from 'util'
 
 const LOG = debuglog('doc')
 
-export const replacer = (match, table) => {
-  const t = table.trim()
+export function replacer(match, macro, table) {
+  const { tableMacros = {} } = this
+  const macroFn = tableMacros[macro]
   try {
-    const res = JSON.parse(t)
+    const res = JSON.parse(table)
     const [header, ...rows] = res
+    const realRows = macroFn ? rows.map(macroFn) : rows
     const a = [
       getRow(header),
       getRow(header.map(({ length }) => '-'.repeat(length))),
-      ...rows.map(getRow),
+      ...realRows.map(getRow),
     ]
     return a.join('\n')
   } catch (err) {
@@ -24,7 +26,7 @@ const getRow = (row) => {
   return s
 }
 
-const re = /```table([\s\S]+?)```/mg
+const re = /```table(?: +(.+) *)?\n([\s\S]+?)\n```$/mg
 
 const tableRule = {
   re,
