a2c6133e39135f05f6873f1bab47efd564bba777
Anton <anton@adc.sh>
2018-09-14 10:36:05 +0300

    refactor Toc into ChunkReplaceable

--- src/lib/Toc.js
+++ src/lib/Toc.js
@@ -18,71 +18,174 @@ const re = /(?:^|\n) *(#+) +(.+)/g
 
 const underline = /^ *([=-]+) *$/gm
 
-const getBuffer = async (buffer) => {
-  const {
-    title, methodTitle, code, innerCode, table, linkTitle,
-  } = makeMarkers({
-    title: /^ *#+.+/gm,
-    methodTitle: methodTitleRe,
-    code: codeRe,
-    innerCode: innerCodeRe,
-    table: tableRe,
-    linkTitle: linkTitleRe,
-  })
-
-  const [
-    cutTitle, cutLinkTitle, cutCode, cutMethodTitle,
-    cutInnerCode, cutTable,
-  ] =
-    [title, linkTitle, code, methodTitle, innerCode, table].map((marker) => {
-      const rule = makeCutRule(marker)
-      return rule
-    })
-  const [
-    insertTitle, insertLinkTitle, insertMethodTitle, insertTable,
-  ] =
-    [title, linkTitle, methodTitle, table].map((marker) => {
-      const rule = makePasteRule(marker)
-      return rule
+class ChunkReplaceable extends Replaceable {
+  /**
+   * A chunk replaceable needs to be used because we want to process the whole chunk from pedantry first because we will need to sort titles as titles are detected rule-by-rule and not in the natural order.
+   * @constructor
+   */
+  constructor(skipLevelOne) {
+    const {
+      methodTitle, code, innerCode, table,
+    } = makeMarkers({
+      methodTitle: methodTitleRe,
+      code: codeRe,
+      innerCode: innerCodeRe,
+      table: tableRe,
+      linkTitle: linkTitleRe,
     })
 
-  const rs = new Replaceable([
-    cutInnerCode, // this ensures no link titles are detected inside of inner code
-    // cutTitle, // i don't know why we are doing this
-    // cutLinkTitle,
+    const [
+      cutCode, cutMethodTitle,
+      cutInnerCode, cutTable,
+    ] =
+      [code, methodTitle, innerCode, table].map((marker) => {
+        const rule = makeCutRule(marker)
+        return rule
+      })
+    const [
+      insertMethodTitle, insertTable,
+    ] =
+      [methodTitle, table].map((marker) => {
+        const rule = makePasteRule(marker)
+        return rule
+      })
 
-    // make sure those are not cut with code
-    cutTable,
-    cutMethodTitle,
+    const getTitle = (title) => {
+      const t = title.replace(innerCode.regExp, (m, i) => {
+        const val = innerCode.map[i]
+        return val
+      })
+      return t
+    }
 
-    // never pasted back
-    cutCode,
-    stripComments,
+    const rules = [
+      cutInnerCode, // this ensures no link titles are detected inside of inner code
 
-    // types will add link titles
-    typedefMdRule,
-    typeRule,
+      // make sure those are not cut with code
+      cutTable,
+      cutMethodTitle,
 
-    // paste those cut out earlier.
-    insertMethodTitle,
-    insertTable,
+      // never pasted back
+      cutCode,
+      stripComments,
 
-    macroRule,
-    tableRule,
-    // insertLinkTitle,
-    // insertTitle,
-    // {
-    //   re: /[\s\S]*/,
-    //   replacement(match) {
-    //     debugger
-    //     return match
-    //   },
-    // },
-  ])
-  rs.end(buffer)
-  const b = await collect(rs)
-  // console.log(underlined.map)
-  return { b, innerCode }
+      // types will add link titles
+      typedefMdRule,
+      typeRule,
+
+      // paste those cut out earlier.
+      insertMethodTitle,
+      insertTable,
+
+      macroRule,
+      tableRule,
+      {
+        re: underline,
+        replacement(match, u, position, input) {
+          const level = u.indexOf('-') + 2 // either 0 or -1
+          if (this.skipLine(level)) return match
+          const lines = []
+          let ok = true
+          let s = input.substr(0, position - 1)
+          while (ok) {
+            const li = s.lastIndexOf('\n')
+            const t = s.substr(li + 1)
+            const isLine = new RegExp(underline.source).test(t)
+            if (isLine) {
+              break
+            }
+            ok = /^ *(?!\s*(?:>|(?:[+*-] )|(?:\d+\.)|(?:# )|`{3,}))[^\s]+.*$/.test(t)
+            if (ok) {
+              lines.unshift(t)
+              s = s.substr(0, s.length - t.length - 1)
+            } else {
+              break
+            }
+          }
+          const title = `${lines.map(l => l.trim()).join('<br/>')}`
+          if (!title) return match
+          const t = getTitle(title)
+          const link = getLink(t)
+          this.emit('title', {
+            title: t, link, level, position,
+          })
+          return match
+        },
+      },
+      {
+        re,
+        replacement(match, { length: level }, title, position) {
+          if (this.skipLine(level)) return match
+          const t = getTitle(title)
+
+          this.emit('title', {
+            title: t,
+            link: getLink(t),
+            position,
+            level,
+          })
+          return match
+        },
+      },
+      {
+        re: methodTitleRe,
+        replacement(match, hash, isAsync, name, returnType, jsonArgs, position) {
+          try {
+            const { length: level } = hash
+
+            if (this.skipLine(level)) return match
+            const json = jsonArgs.trim() ? jsonArgs : '[]'
+            const bb = [isAsync, name]
+              .filter(a => a)
+              .join(' ').trim()
+            const args = JSON.parse(json)
+            const s = args.map(([argName, type, shortType]) => {
+              let tt
+              if (shortType) tt = shortType
+              else if (typeof type == 'string') tt = type
+              else tt = 'object'
+              return `${argName}: ${tt}`
+            })
+            const fullTitle = replaceTitle(hash, isAsync, name, returnType, jsonArgs)
+              .replace(/^#+ +/, '')
+            const link = getLink(fullTitle)
+
+            const rt = `${returnType ? `: ${returnType}` : ''}`
+            const title = `\`${bb}(${s.join(', ')})${rt}\``
+            this.emit('title', {
+              title,
+              link,
+              position,
+              level,
+            })
+          } catch (err) {
+            // ok
+            return match
+          }
+        },
+      },
+      {
+        re: linkTitleRe,
+        replacement(match, title, l, position) {
+          const t = getTitle(title)
+          const link = getLink(t)
+          this.emit('title', {
+            title: t,
+            ...(l == 't' ? { parentLevel: true } : { level: l.length }),
+            link,
+            position,
+          })
+          return match
+        },
+      },
+    ]
+    super(rules)
+    this.skipLevelOne = skipLevelOne
+  }
+
+  skipLine(level) {
+    return this.skipLevelOne && level == 1
+  }
 }
 
 export default class Toc extends Transform {
@@ -95,6 +198,7 @@ export default class Toc extends Transform {
     const {
       skipLevelOne = true,
     } = config
+
     super()
     this.skipLevelOne = skipLevelOne
     this.level = 0
@@ -121,137 +225,30 @@ export default class Toc extends Transform {
     return ts
   }
 
-  skipLine(level) {
-    return this.skipLevelOne && level == 1
-  }
-
   async _transform(buffer, enc, next) {
-    const { b, innerCode } = await getBuffer(buffer)
+    const cr = new ChunkReplaceable(this.skipLevelOne)
+    cr
+      .on('title', t => this.addTitle(t))
+      .end(buffer)
 
-    const getTitle = (title) => {
-      const t = title.replace(innerCode.regExp, (m, i) => {
-        const val = innerCode.map[i]
-        return val
+    try {
+      await collect(cr)
+      const sorted = this.sortTitles()
+      sorted.forEach((title) => {
+        if (title.parentLevel) {
+          title.level = this.level + 1
+        } else {
+          this.level = title.level
+        }
+        const line = this.getTocLine(title)
+        this.push(line)
+        this.push('\n')
       })
-      return t
+      this.clear()
+      next()
+    } catch (err) {
+      next(err)
     }
-
-    const replaceable = new Replaceable([
-      {
-        re: underline,
-        replacement: (match, u, position, input) => {
-          const level = u.indexOf('-') + 2 // either 0 or -1
-          if (this.skipLine(level)) return match
-          const lines = []
-          let ok = true
-          let s = input.substr(0, position - 1)
-          while (ok) {
-            const li = s.lastIndexOf('\n')
-            const t = s.substr(li + 1)
-            const isLine = new RegExp(underline.source).test(t)
-            if (isLine) {
-              break
-            }
-            ok = /^ *(?!\s*(?:>|(?:[+*-] )|(?:\d+\.)|(?:# )|`{3,}))[^\s]+.*$/.test(t)
-            if (ok) {
-              lines.unshift(t)
-              s = s.substr(0, s.length - t.length - 1)
-            } else {
-              break
-            }
-          }
-          const title = `${lines.map(l => l.trim()).join('<br/>')}`
-          if (!title) return match
-          const t = getTitle(title)
-          const link = getLink(t)
-          this.addTitle({
-            title: t, link, level, position,
-          })
-          return match
-        },
-      },
-      {
-        re,
-        replacement: (match, { length: level }, title, position) => {
-          if (this.skipLine(level)) return match
-          const t = getTitle(title)
-
-          this.addTitle({
-            title: t,
-            link: getLink(t),
-            position,
-            level,
-          })
-          return match
-        },
-      },
-      {
-        re: methodTitleRe,
-        replacement: (match, hash, isAsync, name, returnType, jsonArgs, position) => {
-          try {
-            const { length: level } = hash
-
-            if (this.skipLine(level)) return match
-            const json = jsonArgs.trim() ? jsonArgs : '[]'
-            const bb = [isAsync, name]
-              .filter(a => a)
-              .join(' ').trim()
-            const args = JSON.parse(json)
-            const s = args.map(([argName, type, shortType]) => {
-              let tt
-              if (shortType) tt = shortType
-              else if (typeof type == 'string') tt = type
-              else tt = 'object'
-              return `${argName}: ${tt}`
-            })
-            const fullTitle = replaceTitle(hash, isAsync, name, returnType, jsonArgs)
-              .replace(/^#+ +/, '')
-            const link = getLink(fullTitle)
-
-            const rt = `${returnType ? `: ${returnType}` : ''}`
-            const title = `\`${bb}(${s.join(', ')})${rt}\``
-            this.addTitle({
-              title,
-              link,
-              position,
-              level,
-            })
-          } catch (err) {
-            // ok
-            return match
-          }
-        },
-      },
-      {
-        re: linkTitleRe,
-        replacement: (match, title, l, position) => {
-          const t = getTitle(title)
-          const link = getLink(t)
-          this.addTitle({
-            title: t,
-            ...(l == 't' ? { parentLevel: true } : { level: l.length }),
-            link,
-            position,
-          })
-          return match
-        },
-      },
-    ])
-    replaceable.end(b)
-    await collect(replaceable)
-    const sorted = this.sortTitles()
-    sorted.forEach((title) => {
-      if (title.parentLevel) {
-        title.level = this.level + 1
-      } else {
-        this.level = title.level
-      }
-      const line = this.getTocLine(title)
-      this.push(line)
-      this.push('\n')
-    })
-    this.clear()
-    next()
   }
   clear() {
     this.titles = []
