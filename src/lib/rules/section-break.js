5da561fe69130e5d1640649ca7cdb1836eab48a1
Anton <anton@adc.sh>
2018-09-15 07:12:19 +0300

    i and use clone

--- src/lib/rules/section-break.js
+++ src/lib/rules/section-break.js
@@ -1,7 +1,10 @@
-import ensurePath from '@wrote/ensure-path'
 import { join } from 'path'
-import { createReadStream, createWriteStream } from 'fs'
 import mismatch from 'mismatch'
+import clone from '@wrote/clone'
+import { debuglog } from 'util'
+import { c, b } from 'erte'
+
+const LOG = debuglog('doc')
 
 const sectionBrakeRe = /^%~(?: +(-?\d+))?(?: +(.+))?%$/gm
 
@@ -9,20 +12,32 @@ const rule = {
   re: sectionBrakeRe,
   async replacement(match, number, attrs = '') {
     let n = 0
-    if (number) n = parseInt(number)
-    else if (this.sectionBrakeNumber !== undefined) n = this.sectionBrakeNumber == 22
-      ? 0                           // reset
-      : this.sectionBrakeNumber + 1 // increase from the prev one
-    const isEnd = n >= 0
-    if (isEnd) this.sectionBrakeNumber = n
-    const name = `${n}.svg`
-    const imgPath = join(__dirname, '../../section-breaks', name)
-    const newPath = getNewPath(name)
-    await copy(imgPath, newPath)
-    const a = mismatch(/(.+)="(.+)"/gm, attrs, ['key', 'val'])
-      .reduce((acc, { key, val }) => ({ ...acc, [key]: val }), {})
-    const tags = getTags({ src: newPath, ...a })
-    return tags
+    try {
+      if (number) n = parseInt(number)
+      else if (this.sectionBrakeNumber !== undefined) n = this.sectionBrakeNumber == 22
+        ? 0                           // reset
+        : this.sectionBrakeNumber + 1 // increase from the prev one
+      const isEnd = n >= 0
+      if (isEnd) this.sectionBrakeNumber = n
+      const name = `${n}.svg`
+      const imgPath = join(__dirname, '../../section-breaks', name)
+      const { to, ...a } = mismatch(/(.+)="(.+)"/gm, attrs, ['key', 'val'])
+        .reduce((acc, { key, val }) => ({ ...acc, [key]: val }), {
+          to: '.documentary/section-breaks',
+        })
+
+      await clone(imgPath, to)
+
+      const tags = getTags({ src: join(to, name), ...a })
+      return tags
+    } catch (err) {
+      const h = c(err.message, 'red')
+      const [, ...s] = err.stack.split('\n')
+      const st = b(s.join('\n'), 'red')
+      const l = `Section break ${n}: ${h}\n${st}`
+      LOG(l)
+      return match
+    }
   },
 }
 
@@ -33,20 +48,6 @@ const getTags = ({
   const a = Object.keys(attrs).map(k => `${k}="${attrs[k]}"`).join(' ')
   const s = `<p align="center"><a href="${href}"><img ${a}></a></p>`
   return s
-}
-
-const getNewPath = (name) => {
-  const j = join('.documentary/section-breaks', name)
-  return j
-}
-
-const copy = async (from, newPath) => {
-  await ensurePath(newPath)
-  const rs = createReadStream(from)
-  const ws = createWriteStream(newPath)
-  await new Promise((r, rj) => {
-    rs.pipe(ws).on('close', r).on('error', rj)
-  })
 }
 
 export { sectionBrakeRe }
