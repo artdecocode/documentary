9e2367ecb015c6e60645833aa1c06777d75f893e
Anton <anton@adc.sh>
2019-03-24 04:42:24 +0300

    bin tests

--- test/spec/bin/default.js
+++ test/spec/bin/default.js
@@ -1,19 +1,29 @@
 import { ok } from 'assert'
 import TempContext from 'temp-context'
+import Zoroaster from 'zoroaster'
 import Context from '../../context'
 
-/** @type {Object.<string, (c: Context, t:TempContext)>} */
+/** @type {Object.<string, (c: Context, z:Zoroaster, t:TempContext)>} */
 const T = {
-  context: [Context, TempContext],
-  async 'generates correct markdown'({ doc, README_PATH }) {
+  context: [Context, Zoroaster, TempContext],
+  async 'generates correct markdown'({ doc, README_PATH }, { snapshotExtension }) {
     const { stdout } = await doc(README_PATH)
+    snapshotExtension('md')
     return stdout.trim()
   },
-  async '!generates correct markdown and saves it to a file'({ doc, README_PATH }, { TEMP, snapshot }) {
-    const { stdout } = await doc(README_PATH, '-o', `${TEMP}/output.md`)
+  async 'generates correct markdown and saves it to a file'({ doc, README_PATH }, { snapshotSource }, { read, resolve }) {
+    const p = resolve('output.md')
+    const { stdout } = await doc(README_PATH, '-o', p)
     ok(/Saved/.test(stdout))
-    const s = await snapshot('output.md')
+    const s = await read('output.md')
+    snapshotSource('generates correct markdown', 'md')
     return s.trim()
+  },
+  async 'generates JSDoc to stdout'({ doc, generateImports }, { snapshotExtension }) {
+    const { stdout } = await doc(generateImports, '-g', '-')
+    const res = stdout.trim()
+    snapshotExtension('js')
+    return res
   },
 }
 
