579aa981be56d0a572da5aec916c672034c99831
Anton <anton@adc.sh>
2019-04-29 13:13:34 +0300

    b

--- build/bin/get-args.js
+++ build/bin/get-args.js
@@ -37,7 +37,6 @@ let argufy = require('argufy'); if (argufy && argufy.__esModule) argufy = argufy
   },
   'namespace': {
     description: 'The root namespace: types within it will not be printed\nwith their namespace prefix.',
-    boolean: true,
     short: 'n',
   },
   'push': {
@@ -109,7 +108,7 @@ const args = argufy(argsConfig)
  * The root namespace: types within it will not be printed
     with their namespace prefix.
  */
-       const _namespace = /** @type {boolean} */ (args['namespace'])
+       const _namespace = /** @type {string} */ (args['namespace'])
 
 /**
  * Starts _Documentary_ in watch mode. After changes are
