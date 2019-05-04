3b48bb0d3622d8aa83d28478b9e54f3112a1ee39
Anton <anton@adc.sh>
2019-05-04 19:38:19 +0300

    rm jsx build

--- build/components/index.jsx
+++
@@ -1,8 +0,0 @@
-const $_shell = require('./shell');
-const $_Argufy = require('./Argufy');
-const $_Html = require('./Html');
-
-
-module.exports.shell = $_shell
-module.exports.argufy = $_Argufy
-module.exports.md2html = $_Html
