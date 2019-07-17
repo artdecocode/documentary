13f0b2f464f0ebbb77b773812910454dfdc82976
Anton <anton@adc.sh>
2019-07-17 13:48:16 +0300

    relative arg 2 fork

--- src/lib/rules/fork.js
+++ src/lib/rules/fork.js
@@ -84,10 +84,11 @@ const doFork = async (old, mod, args) => {
 }
 
 const forkRule = {
-  re: /( *)%([!_]+)?FORK(ERR)?(?:-(\w+))? (.+)%/mg,
+  re: /( *)%([/!_]+)?FORK(ERR)?(?:-(\w+))? (.+)%/mg,
   async replacement(match, ws, service, err, lang, m) {
     const noCache = /!/.test(service) || this.noCache
     const old = /_/.test(service)
+    const relative = /\//.test(service)
     try {
       let awaited = false
       const q = queue[m]
@@ -100,6 +101,7 @@ const forkRule = {
       queue[m] = { promise, err }
       let res = await promise
       if (ws) res = res.replace(/^/gm, ws)
+      if (relative) res = res.replace(new RegExp(`${process.cwd()}/?`, 'g'), '')
       return res
     } catch (e) {
       this.log(c(`FORK ${m} error`, 'red'))
