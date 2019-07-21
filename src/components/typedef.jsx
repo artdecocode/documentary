24b46bb09c8b056c45c6905fe126f3551645f0d0
Anton <anton@adc.sh>
2019-07-21 21:51:42 +0300

    typedef

--- src/components/typedef.jsx
+++ src/components/typedef.jsx
@@ -5,8 +5,22 @@ export default function typedef({ documentary: { locations, allTypes }, children
     throw new Error(`No types for location ${location}`, )
   }
   const t = name ? types.filter(a => a.name == name) : types
-  const res = t.map((type) => {
-    return type.toMarkdown(allTypes, { narrow, flatten })
-  }).join('\n\n')
+
+  const typesToMd = t.filter(({ import: i }) => !i)
+  let flattened = {}
+  const tt = typesToMd.map(i => i.toMarkdown(allTypes, { narrow, flatten(n) {
+    flattened[n] = true
+  } }))
+  // found those imports that will be flattened
+  const importsToMd = t
+    .filter(({ import: i }) => i)
+    .filter(({ fullName }) => !(fullName in flattened))
+  const j = importsToMd.map(i => i.toMarkdown(allTypes, { narrow, flatten }))
+
+  const res = [
+    ...tt,
+    ...j,
+  ].join('\n\n')
+
   return res
 }
